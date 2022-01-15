f67aa15d35584f7c07512f30d2b833f80ff8cfda
Maxwell Lang <maxwlang@gmail.com>
2022-01-15 16:25:47 -0600

    push changes

--- src/events/readySyncHistory.js
+++ src/events/readySyncHistory.js
@@ -1,8 +1,20 @@
+const bPromise = require('bluebird')
+
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(bot) {
+	async execute(bot) {
         bot.log.info('Syncing react history..')
-        // console.log(bot.channels)
+		const server = bot.guilds.cache.get(bot.config.server.id)
+		let channels = server.channels.cache
+
+		if (bot.config.server.channel.monitorChannelIDs !== null) {
+			channels = await bPromise.filter(channels, channel => {
+				console.log(channel.id)
+				return bot.config.server.channel.monitorChannelIDs.indexOf(channel.id) !== -1
+			})
+		}
+
+        console.log(channels.forEach(channel => channel.id))
 	},
 }
