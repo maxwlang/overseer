6809a151810e638177f38cc1ef4585fce0cdaa1d
Maxwell Lang <maxwlang@gmail.com>
2022-01-16 04:21:49 -0600

    clean up load message

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@
         logger.info('Loading database..')
         await db.sequelize.authenticate()
 
-        logger.info(`Loaded ${await db.sequelize.models.User_Reacts.count()} reacts for ${await db.sequelize.models.User.count()} users.`)
-        
+        const {User_Reacts, User, Emote} = db.sequelize.models
+        logger.info(`Loaded ${await Emote.count()} emotes, ${await User_Reacts.count()} reacts, and ${await User.count()} users.`)
+
         logger.info('Loading bot')
         await require('./bot')(config, db, logger)
     } catch (e) {
