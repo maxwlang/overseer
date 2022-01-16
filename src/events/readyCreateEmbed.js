ebbfc311c86a2617f30b008b5cfa02aad2b7bb59
Maxwell Lang <maxwlang@gmail.com>
2022-01-16 04:22:05 -0600

    Bring embed creation back

--- src/events/readyCreateEmbed.js
+++ src/events/readyCreateEmbed.js
@@ -7,11 +7,11 @@ module.exports = {
 		bot.log.info('(Re)creating discord embed')
 
 		// Delete previous messages in this channel. Ideally only the bot posts here.
-		// const channel = bot.channels.cache.get(bot.config.server.channel.statsChannelID)
-		// channel.messages.fetch({limit: 100})
-		// 	.then(messages => messages.forEach(message => message.delete()))
+		const channel = bot.channels.cache.get(bot.config.server.channel.statsChannelID)
+		channel.messages.fetch({limit: 100})
+			.then(messages => messages.forEach(message => message.delete()))
 
-		// const statusEmbed = await createStatusEmbed(bot, bot.config.emoji.watching)
-		// bot.statusEmbed = await channel.send(statusEmbed)
+		const statusEmbed = await createStatusEmbed(bot, bot.config.emoji.watching)
+		bot.statusEmbed = await channel.send(statusEmbed)
 	}
 }
