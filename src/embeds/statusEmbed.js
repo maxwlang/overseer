fffa58425837d48da25e5ab9373deccc2bac6ad3
Maxwell Lang <maxwlang@gmail.com>
2022-01-17 04:27:52 -0600

    Leaderboard support custom emotes

--- src/embeds/statusEmbed.js
+++ src/embeds/statusEmbed.js
@@ -5,17 +5,34 @@ module.exports = async (bot, emojis, selected = 0) => {
     const botName = bot.user.username
     const avatarUrl = bot.user.displayAvatarURL({ dynamic: true })
     const serverName = bot.guilds.cache.get(bot.config.server.id)
-    const { User_Reacts, User } = bot.db.sequelize.models
+    const { User_Reacts, User, Emote } = bot.db.sequelize.models
 
     bot.statusEmbedIndex = selected
     const emoji = emojis[selected]
 
     const users = await User.findAll()
     const leaderboard = (await bPromise.map(users, async user => {
+        // Try and find emoji reference
+        const server = bot.guilds.cache.get(bot.config.server.id)
+        const customEmoji = server.emojis.cache.find(cacheEmoji => cacheEmoji.name === emoji)
+        const isCustom = customEmoji !== undefined
+
+        const [emojiDetails] = await Emote.findAll({
+            where: (() => {
+                const query = { isCustom }
+                if (isCustom) query.customId = customEmoji.id
+                if (!isCustom) query.unicodeValue = emoji
+                return query
+            })()
+        })
+
+        // console.log(customEmoji)
+
         const [userTotals] = await User_Reacts.count({
             group: 'userUuid',
             where: {
                 userUuid: user.dataValues.uuid,
+                emoteUuid: emojiDetails.dataValues.uuid
                 // also include emote details here
             },
         })
@@ -54,13 +71,23 @@ module.exports = async (bot, emojis, selected = 0) => {
     for (let i = 0; i < bot.config.leaderboard.maxUsers; i++) {
         try {
             await bot.users.fetch(leaderboard[i].snowflake)
-                .then(user => embed.addFields(
-                    {
-                        name: `${i + 1}. ${user.username}`,
-                        value: `${emoji} - ${leaderboard[i].total}`,
-                        inline: true,
-                    },
-                ))
+                // eslint-disable-next-line no-loop-func
+                .then(user => {
+                    const server = bot.guilds.cache.get(bot.config.server.id)
+                    const customEmoji = server.emojis.cache.find(cacheEmoji => cacheEmoji.name === emoji)
+                    const isCustom = customEmoji !== undefined
+                    const visualEmoji = !isCustom ? emoji : customEmoji.toString()
+
+                    if (leaderboard[i].total === 0) return
+
+                    embed.addFields(
+                        {
+                            name: `${i + 1}. ${user.username}`,
+                            value: `${visualEmoji} - ${leaderboard[i].total}`,
+                            inline: true,
+                        },
+                    )
+                })
                 .catch(e => {
                     if (e.code === 10013 && leaderboard[i].snowflake !== undefined) return
                     throw e
@@ -81,12 +108,29 @@ module.exports = async (bot, emojis, selected = 0) => {
         { name: '\u200B', value: '\u200B' },
     )
 
-    const selectOptions = await bPromise.map(emojis, (emoji, index) => ({
-        label: `${emoji} Leaderboard`,
-        description: `User leaderboard for the ${emoji} emoji.`,
-        value: index.toString(),
-        default: selected > 0 ? index === selected : index === 0,
-    }))
+    const selectOptions = await bPromise.map(emojis, async (emoji, index) => {
+        const server = bot.guilds.cache.get(bot.config.server.id)
+        const customEmoji = server.emojis.cache.find(cacheEmoji => cacheEmoji.name === emoji)
+        const isCustom = customEmoji !== undefined
+
+        const [emojiDetails] = await Emote.findAll({
+            where: (() => {
+                const query = { isCustom }
+                if (isCustom) query.customId = customEmoji.id
+                if (!isCustom) query.unicodeValue = emoji
+                return query
+            })()
+        })
+
+        const visualEmoji = isCustom ? emoji : emojiDetails.dataValues.unicodeValue
+
+        return {
+            label: `${visualEmoji} Leaderboard`,
+            description: `User leaderboard for the ${visualEmoji} emoji.`,
+            value: index.toString(),
+            default: selected > 0 ? index === selected : index === 0,
+        }
+    })
 
     const row = new MessageActionRow()
         .addComponents(
